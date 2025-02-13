c1da3f66d7b3240f2ddefaa2b9a2bf8e99821d9d
pagalVivek <naruto567uzumaki009@gmail.com>
2025-02-13 15:47:55 +0530

    checking my published packafes

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-function add(a, b) {
-  return `Addition: The sum of ${a} and ${b} is ${a + b}`;
-}
+// function add(a, b) {
+//   return `Addition: The sum of ${a} and ${b} is ${a + b}`;
+// }
 
-function subtract(a, b) {
-  return `Subtraction: The difference between ${a} and ${b} is ${a - b}`;
-}
+// function subtract(a, b) {
+//   return `Subtraction: The difference between ${a} and ${b} is ${a - b}`;
+// }
 
-function multiply(a, b) {
-  return `Multiplication: The product of ${a} and ${b} is ${a * b}`;
-}
+// function multiply(a, b) {
+//   return `Multiplication: The product of ${a} and ${b} is ${a * b}`;
+// }
 
-function divide(a, b) {
-  if (b === 0) {
-    return "Error: Division by zero";
-  }
-  return `Division: The quotient of ${a} and ${b} is ${a / b}`;
-}
-export const calculator = {
-  add,
-  subtract,
-  multiply,
-  divide,
-};
+// function divide(a, b) {
+//   if (b === 0) {
+//     return "Error: Division by zero";
+//   }
+//   return `Division: The quotient of ${a} and ${b} is ${a / b}`;
+// }
+// export const calculator = {
+//   add,
+//   subtract,
+//   multiply,
+//   divide,
+// };
+import {calculator} from 'npm-module-publish';
+
+console.log(calculator.add(1,3));
